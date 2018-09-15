18edcc193df6ce0a45459160c12021bcadb65965
Tommy Le <teknoma104@gmail.com>
2018-09-14 23:26:43 -0500

    Added most of the functionality of the homework, need to adjust some cosmetical features in main.handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('New Bacon-ings Burger');
+INSERT INTO burgers (burger_name) VALUES ('Never Been Feta Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mission A-Corn-Plished Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pepper Don't Preach Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rest in Peas Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Chard To A Crisp Burger');
