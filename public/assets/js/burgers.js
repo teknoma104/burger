a2a2318e27eefcc7355e155ee92411cea187350c
Tommy Le <teknoma104@gmail.com>
2018-09-16 23:44:01 -0500

    Added readme.md and added form validation for when user creates their own burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -10,11 +10,10 @@ $(function () {
 
         console.log("devoured = " + newDevoured);
 
-        if (newDevoured === true)
-        {
-            $("#Bob").prop("src","/assets/img/bob-serve.jpg");
+        if (newDevoured === true) {
+            $("#Bob").prop("src", "/assets/img/bob-serve.jpg");
         } else {
-            $("#Bob").prop("src","/assets/img/bob-angry.jpg");
+            $("#Bob").prop("src", "/assets/img/bob-angry.jpg");
         }
 
         // Send the PUT request.
@@ -34,28 +33,49 @@ $(function () {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
-        var newBurger = {
-            burger_name: $("#burgerName").val().trim(),
-            devoured: $("[name=devoured]:checked").val().trim()
-        };
+        // Form Validation
+        function validateForm() {
+            var isValid = true;
 
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function () {
-                console.log("created new burger");
-                // Reload the page to get the updated list
-                location.reload();
+            // Checks for blank value in the two text input boxes
+            if ($("#burgerName").val() === "") {
+                isValid = false;
             }
-        );
+
+            if ($("[name=devoured]:checked").val() === "undefined") {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        if (validateForm) {
+
+            var newBurger = {
+                burger_name: $("#burgerName").val().trim(),
+                devoured: $("[name=devoured]:checked").val().trim()
+            };
+
+            // Send the POST request.
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(
+                function () {
+                    console.log("created new burger");
+                    // Reload the page to get the updated list
+                    location.reload();
+                }
+            );
+
+        }
+
     });
 
 });
 
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $("#Bob").prop("src","/assets/img/bob-idle.png");
+$(document).ready(function () {
+    console.log("ready!");
+    $("#Bob").prop("src", "/assets/img/bob-idle.png");
 });
